605cf498b1a0bdc42de3abcac9adcf7302206752
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-02-07 00:03:20 -0800

    chore: test and fix dedup-chan

--- crates/rostra-util-dedup-chan/src/lib.rs
+++ crates/rostra-util-dedup-chan/src/lib.rs
@@ -1,17 +1,40 @@
-use std::collections::{HashSet, VecDeque};
+//! Deduplicating multi-channel
+//!
+//! This channel is designed for broadcasting work to potentially multiple
+//! worker threads, where multiple updates should be deduplicated while waiting
+//! for processing.
+//!
+//! To use, first a [`Sender`] is created. Then [`Receiver`]s can be created
+//! by calling [`Sender::subscribe`]. Each subscription creates a separate
+//! channel.
+//!
+//! On [`Receiver::send`] a copy of an item will be addedto each subscribed
+//! channel. If the item is already in the channel, yet unprocessed, it will not
+//! be added again.
+//!
+//! Channels will be destroyed when the last [`Receiver`] is gone and the
+//! channel is "disconnected".
+//!
+//! [`Receivers`] will be notified when the amount of pending work exceeds
+//! `capacity`.
+
+use std::collections::{BTreeMap, HashSet, VecDeque};
 use std::sync::{Arc, Mutex};
 use std::{cmp, fmt, hash};
 
 use snafu::Snafu;
 use tokio::sync::watch;
 
-#[derive(Snafu, Debug)]
+#[derive(Snafu, Debug, PartialEq, Eq)]
 pub enum RecvError {
     Closed,
     Lagging,
 }
 
-pub struct SendError<T>(pub T);
+pub enum SendError<T> {
+    Closed(T),
+    Lagging(T),
+}
 
 impl<T> fmt::Debug for SendError<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -27,22 +50,23 @@ impl<T> fmt::Display for SendError<T> {
 
 impl<T> std::error::Error for SendError<T> {}
 
+/// An inner part of a [`Channel`], shared with the [`Receiver`]s
 #[derive(Clone)]
-struct Inner<T> {
+struct ChannelInner<T> {
     set: HashSet<T>,
     queue: VecDeque<T>,
     dropped_messages: bool,
 }
 
-/// See [`channel`]
+/// A queue in which items are being collected for the
 #[derive(Clone)]
-pub struct Sender<T> {
-    inner: Arc<std::sync::Mutex<Inner<T>>>,
+struct Channel<T> {
+    inner: Arc<std::sync::Mutex<ChannelInner<T>>>,
     tx: watch::Sender<usize>,
     capacity: usize,
 }
 
-impl<T> Sender<T>
+impl<T> Channel<T>
 where
     T: cmp::Eq + hash::Hash + Clone,
 {
@@ -60,29 +84,117 @@ where
         let len = lock.queue.len();
         if self.capacity <= len {
             lock.dropped_messages = true;
-            return Err(SendError(v));
+            return Err(SendError::Lagging(v));
         }
 
         if self.tx.send(len + 1).is_err() {
-            return Err(SendError(v));
+            return Err(SendError::Closed(v));
         };
 
         lock.set.insert(v.clone());
         lock.queue.push_back(v);
         Ok(())
     }
+}
 
-    pub fn subscribe(&self) -> Receiver<T> {
+#[derive(Clone)]
+pub struct Sender<T> {
+    channels: BTreeMap<usize, Channel<T>>,
+}
+
+impl<T> Sender<T>
+where
+    T: cmp::Eq + hash::Hash + Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            channels: Default::default(),
+        }
+    }
+    /// Add a value to the channel
+    ///
+    /// Returns [`SendError`] if the channel is already at full capacity.
+    /// Some receiver will be notified about the dropped message.
+    ///
+    /// Returns number of channel that the message was delivered to.
+    pub async fn send(&mut self, v: T) -> usize {
+        let mut to_delete = vec![];
+        let mut sent_count = 0;
+
+        for (k, inner) in &mut self.channels {
+            match inner.send(v.clone()).await {
+                Ok(_) => {
+                    sent_count += 1;
+                }
+                Err(SendError::Closed(_)) => {
+                    to_delete.push(*k);
+                }
+                Err(SendError::Lagging(_)) => {}
+            }
+        }
+
+        for to_delete in to_delete {
+            self.channels.remove(&to_delete).expect("Must be some");
+        }
+
+        sent_count
+    }
+
+    /// Subscribe to the `Sender`
+    ///
+    /// From now on, a copy of every sent item will be queued to be delivered to
+    /// the returned `Receiver`.
+    pub fn subscribe(&mut self, capacity: usize) -> Receiver<T> {
+        let (sending_tx, sending_rx) = watch::channel(0);
+        let inner = ChannelInner {
+            set: HashSet::new(),
+            queue: VecDeque::new(),
+            dropped_messages: false,
+        };
+
+        let inner = Arc::new(Mutex::new(inner));
+
+        let sender_inner = Channel {
+            tx: sending_tx,
+            inner: inner.clone(),
+            capacity,
+        };
+
+        assert!(self
+            .channels
+            .insert(
+                self.channels
+                    .last_key_value()
+                    .map(|(k, _)| *k + 1)
+                    .unwrap_or_default(),
+                sender_inner,
+            )
+            .is_none());
         Receiver {
-            inner: self.inner.clone(),
-            rx: self.tx.subscribe(),
+            inner,
+            rx: sending_rx,
         }
     }
 }
 
-/// See [`channel`]
+impl<T> Default for Sender<T>
+where
+    T: cmp::Eq + hash::Hash + Clone,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A receiving end of a deduplicated channel
+///
+/// Notably, when `Cloned` the new `Receiver` pulls work from the same
+/// queue, leading to potential load-balancing.
+///
+/// To create a new, independent queue, call [`Sender::subscribe`] instead.
+#[derive(Clone)]
 pub struct Receiver<T> {
-    inner: Arc<Mutex<Inner<T>>>,
+    inner: Arc<Mutex<ChannelInner<T>>>,
     rx: watch::Receiver<usize>,
 }
 
@@ -90,22 +202,23 @@ impl<T> Receiver<T>
 where
     T: cmp::Eq + hash::Hash,
 {
+    #[allow(clippy::await_holding_lock)] // clippy is wrong
     pub async fn recv(&mut self) -> std::result::Result<T, RecvError> {
         loop {
-            if self.rx.wait_for(|len| 0 < *len).await.is_err() {
-                return Err(RecvError::Closed);
-            }
-
             let mut lock = self.inner.lock().expect("Locking error");
             let len = lock.queue.len();
 
             if len == 0 {
-                continue;
-            }
+                if lock.dropped_messages {
+                    lock.dropped_messages = false;
+                    return Err(RecvError::Lagging);
+                }
+                drop(lock);
 
-            if lock.dropped_messages {
-                lock.dropped_messages = false;
-                return Err(RecvError::Lagging);
+                if self.rx.changed().await.is_err() {
+                    return Err(RecvError::Closed);
+                }
+                continue;
             }
 
             let v = lock
@@ -116,32 +229,10 @@ where
             if !lock.set.remove(&v) {
                 panic!("Must have a set element when len > 0");
             }
+            return Ok(v);
         }
     }
 }
 
-/// Deduplicating work channel
-///
-/// This channel is designed for passing work items to multiple worker
-pub fn channel<T>(capacity: usize) -> (Sender<T>, Receiver<T>) {
-    let (sending_tx, sending_rx) = watch::channel(0);
-    let inner = Inner {
-        set: HashSet::new(),
-        queue: VecDeque::new(),
-        dropped_messages: false,
-    };
-
-    let inner = Arc::new(Mutex::new(inner));
-
-    (
-        Sender {
-            tx: sending_tx,
-            inner: inner.clone(),
-            capacity,
-        },
-        Receiver {
-            inner,
-            rx: sending_rx,
-        },
-    )
-}
+#[cfg(test)]
+mod tests;
---
+++ crates/rostra-util-dedup-chan/src/tests.rs
@@ -0,0 +1,136 @@
+use super::*;
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn can_send_a_message() {
+    let mut tx = Sender::new();
+
+    let mut rx = tx.subscribe(10);
+
+    assert_eq!(tx.send(8).await, 1);
+
+    assert_eq!(rx.recv().await, Ok(8));
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn can_detect_tx_drop() {
+    let mut tx = Sender::new();
+
+    let mut rx = tx.subscribe(10);
+
+    assert_eq!(tx.send(8).await, 1);
+
+    assert_eq!(rx.recv().await, Ok(8));
+
+    drop(tx);
+
+    assert_eq!(rx.recv().await, Err(RecvError::Closed));
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn dedups_items_single_rx() {
+    let mut tx = Sender::new();
+
+    let mut rx = tx.subscribe(10);
+
+    assert_eq!(tx.send(8).await, 1);
+    assert_eq!(tx.send(8).await, 1);
+    assert_eq!(tx.send(9).await, 1);
+
+    assert_eq!(rx.recv().await, Ok(8));
+    assert_eq!(rx.recv().await, Ok(9));
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn works_with_multiple() {
+    let mut tx = Sender::new();
+
+    let mut rx1 = tx.subscribe(10);
+    let mut rx2 = tx.subscribe(10);
+    let mut rx3 = tx.subscribe(10);
+
+    assert_eq!(tx.send(8).await, 3);
+
+    assert_eq!(rx1.recv().await, Ok(8));
+    assert_eq!(rx2.recv().await, Ok(8));
+    assert_eq!(rx3.recv().await, Ok(8));
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn dedups_items_with_multiple() {
+    let mut tx = Sender::new();
+
+    let mut rx1 = tx.subscribe(10);
+    let mut rx2 = tx.subscribe(10);
+    let mut rx3 = tx.subscribe(10);
+
+    assert_eq!(tx.send(8).await, 3);
+
+    assert_eq!(rx1.recv().await, Ok(8));
+
+    assert_eq!(tx.send(8).await, 3);
+    assert_eq!(tx.send(9).await, 3);
+
+    assert_eq!(rx1.recv().await, Ok(8));
+    assert_eq!(rx1.recv().await, Ok(9));
+    assert_eq!(rx2.recv().await, Ok(8));
+    assert_eq!(rx2.recv().await, Ok(9));
+    assert_eq!(rx3.recv().await, Ok(8));
+    assert_eq!(rx3.recv().await, Ok(9));
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn can_detect_rx_drop() {
+    let mut tx = Sender::new();
+
+    let mut rx1 = tx.subscribe(10);
+    let rx2 = tx.subscribe(10);
+
+    assert_eq!(tx.send(8).await, 2);
+
+    assert_eq!(rx1.recv().await, Ok(8));
+    drop(rx1);
+    drop(rx2);
+
+    assert_eq!(tx.send(9).await, 0);
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn load_balancing_works() {
+    let mut tx = Sender::new();
+
+    let mut rx1 = tx.subscribe(10);
+    let mut rx1_clone = rx1.clone();
+    let mut rx2 = tx.subscribe(10);
+    let mut rx2_clone = rx2.clone();
+    let mut rx3 = tx.subscribe(10);
+    let mut rx3_clone = rx3.clone();
+
+    assert_eq!(tx.send(8).await, 3);
+
+    assert_eq!(rx1.recv().await, Ok(8));
+
+    assert_eq!(tx.send(8).await, 3);
+    assert_eq!(tx.send(9).await, 3);
+
+    assert_eq!(rx1.recv().await, Ok(8));
+    assert_eq!(rx1_clone.recv().await, Ok(9));
+    assert_eq!(rx2_clone.recv().await, Ok(8));
+    assert_eq!(rx2.recv().await, Ok(9));
+    assert_eq!(rx3_clone.recv().await, Ok(8));
+    assert_eq!(rx3.recv().await, Ok(9));
+}
+
+#[test_log::test(tokio::test(flavor = "multi_thread"))]
+async fn can_detect_lagging() {
+    let mut tx = Sender::new();
+
+    let mut rx = tx.subscribe(1);
+
+    assert_eq!(tx.send(8).await, 1);
+    assert_eq!(tx.send(9).await, 0);
+
+    assert_eq!(rx.recv().await, Ok(8));
+    assert_eq!(rx.recv().await, Err(RecvError::Lagging));
+    assert_eq!(tx.send(10).await, 1);
+    assert_eq!(rx.recv().await, Ok(10));
+}
