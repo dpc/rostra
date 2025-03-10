eae8c39a1c711ded3ea3524efbe580970acd0c51
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-03-09 19:19:13 -0700

    chore: yt video embedding

--- crates/rostra-web-ui/src/routes/content.rs
+++ crates/rostra-web-ui/src/routes/content.rs
@@ -1,9 +1,11 @@
+use std::borrow::Cow;
 use std::str::FromStr as _;
 
 use jotdown::{Attributes, Container, Event};
 use maud::{Markup, PreEscaped};
 use rostra_client::ClientRef;
 use rostra_core::id::RostraId;
+use url::Url;
 
 use crate::UiState;
 
@@ -27,6 +29,7 @@ impl UiState {
         });
 
         let mut in_profile_link = vec![];
+        let mut in_img_to_raw_html = vec![];
         let out = jotdown::html::render_to_string(sanitized.flat_map(|event| match event {
             Event::Start(Container::Link(s, jotdown::LinkType::AutoLink), attr) => {
                 if let Some(rostra_id) = Self::extra_rostra_id_link(&s) {
@@ -64,28 +67,49 @@ impl UiState {
                 }
             }
             Event::Start(Container::Image(s, link_type), _attr) => {
-                vec![
-                    Event::Start(
-                        Container::Div {
-                            class: "lazyload-wrapper",
-                        },
-                        jotdown::Attributes::try_from(
-                            "{ onclick=\"this.classList.toggle('-expanded')\" }",
-                        )
-                        .expect("Can't fail"),
-                    ),
-                    Event::Start(Container::Paragraph, Attributes::new()),
-                    Event::Str(format!("Load: {}", s).into()),
-                    Event::End(Container::Paragraph),
-                    Event::Start(
+                let load_msg = format!("Load: {}", s).into();
+                let img_events = if let Some(html) = maybe_embed_media_html(&s) {
+                    in_img_to_raw_html.push(html.clone());
+
+                    vec![
+                        Event::Start(
+                            Container::RawInline { format: "html" },
+                            Attributes::try_from("{ loading=lazy }").expect("Can't fail"),
+                        ),
+                        Event::Str(html.into()),
+                    ]
+                } else {
+                    vec![Event::Start(
                         Container::Image(s, link_type),
                         Attributes::try_from("{ loading=lazy }").expect("Can't fail"),
-                    ),
+                    )]
+                };
+                [
+                    vec![
+                        Event::Start(
+                            Container::Div {
+                                class: "lazyload-wrapper",
+                            },
+                            jotdown::Attributes::try_from(
+                                "{ onclick=\"this.classList.toggle('-expanded')\" }",
+                            )
+                            .expect("Can't fail"),
+                        ),
+                        Event::Start(Container::Paragraph, Attributes::new()),
+                        Event::Str(load_msg),
+                        Event::End(Container::Paragraph),
+                    ],
+                    img_events,
                 ]
+                .concat()
             }
             Event::End(Container::Image(s, link_type)) => {
                 vec![
-                    Event::End(Container::Image(s, link_type)),
+                    if let Some(_html) = in_img_to_raw_html.pop() {
+                        Event::End(Container::RawInline { format: "html" })
+                    } else {
+                        Event::End(Container::Image(s, link_type))
+                    },
                     Event::End(Container::Div {
                         class: "img-wrapper",
                     }),
@@ -108,6 +132,35 @@ impl UiState {
         } else {
             None
         }
+    }
+}
+
+enum ExternalMedia<'s> {
+    YT(Cow<'s, str>),
+}
+fn extract_media(url: &Url) -> Option<ExternalMedia> {
+    let host_str = url.host_str()?;
+    match host_str {
+        "youtube.com" | "www.youtube.com" => {
+            let vid = url.query_pairs().find(|(k, _)| k == "v")?.1;
+
+            Some(ExternalMedia::YT(vid))
+        }
+        _ => None,
+    }
+}
+
+fn maybe_embed_media_html(s: &str) -> Option<String> {
+    let Ok(url) = Url::parse(s) else {
+        return None;
+    };
+
+    match extract_media(&url)? {
+        ExternalMedia::YT(vid) => Some(format!(
+            "<iframe loading=lazy width=\"100%\" style=\"aspect-ratio: 16 / 9;\"
+ src=\"https://www.youtube.com/embed/{}\" frameborder=\"0\"></iframe>",
+            vid
+        )),
     }
 }
 
