9cbe01c0fb237c337c39fc49f441227d294be846
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-03-09 20:29:17 -0700

    chore: embedding external media improvements

--- crates/rostra-web-ui/src/routes/content.rs
+++ crates/rostra-web-ui/src/routes/content.rs
@@ -30,96 +30,123 @@ impl UiState {
 
         let mut in_profile_link = vec![];
         let mut in_img_to_raw_html = vec![];
-        let out = jotdown::html::render_to_string(sanitized.flat_map(|event| match event {
-            Event::Start(Container::Link(s, jotdown::LinkType::AutoLink), attr) => {
-                if let Some(rostra_id) = Self::extra_rostra_id_link(&s) {
-                    // TODO: blocked on https://github.com/hellux/jotdown/issues/86
-                    // let x = client
-                    //     .db()
-                    //     .get_social_profile(rostra_id)
-                    //     .await
-                    //     .map(|record| record.display_name)
-                    //     .unwrap_or_else(|| rostra_id.to_string());
-                    in_profile_link.push(rostra_id);
-                    vec![Event::Start(
-                        Container::Link(
+        let mut in_img_to_img = vec![];
+        let out = jotdown::html::render_to_string(sanitized.flat_map(|event| {
+            match event {
+                Event::Start(Container::Link(s, jotdown::LinkType::AutoLink), attr) => {
+                    if let Some(rostra_id) = Self::extra_rostra_id_link(&s) {
+                        // TODO: blocked on https://github.com/hellux/jotdown/issues/86
+                        // let x = client
+                        //     .db()
+                        //     .get_social_profile(rostra_id)
+                        //     .await
+                        //     .map(|record| record.display_name)
+                        //     .unwrap_or_else(|| rostra_id.to_string());
+                        in_profile_link.push(rostra_id);
+                        vec![Event::Start(
+                            Container::Link(
+                                format!("/ui/profile/{rostra_id}").into(),
+                                jotdown::LinkType::Span(jotdown::SpanLinkType::Inline),
+                            ),
+                            attr,
+                        )]
+                    } else {
+                        vec![Event::Start(
+                            Container::Link(s, jotdown::LinkType::AutoLink),
+                            attr,
+                        )]
+                    }
+                }
+                Event::End(Container::Link(s, jotdown::LinkType::AutoLink)) => {
+                    if let Some(rostra_id) = Self::extra_rostra_id_link(&s) {
+                        in_profile_link.pop();
+                        vec![Event::End(Container::Link(
                             format!("/ui/profile/{rostra_id}").into(),
                             jotdown::LinkType::Span(jotdown::SpanLinkType::Inline),
-                        ),
-                        attr,
-                    )]
-                } else {
-                    vec![Event::Start(
-                        Container::Link(s, jotdown::LinkType::AutoLink),
-                        attr,
-                    )]
-                }
-            }
-            Event::End(Container::Link(s, jotdown::LinkType::AutoLink)) => {
-                if let Some(rostra_id) = Self::extra_rostra_id_link(&s) {
-                    in_profile_link.pop();
-                    vec![Event::End(Container::Link(
-                        format!("/ui/profile/{rostra_id}").into(),
-                        jotdown::LinkType::Span(jotdown::SpanLinkType::Inline),
-                    ))]
-                } else {
-                    vec![Event::End(Container::Link(s, jotdown::LinkType::AutoLink))]
-                }
-            }
-            Event::Start(Container::Image(s, link_type), _attr) => {
-                let load_msg = format!("Load: {}", s).into();
-                let img_events = if let Some(html) = maybe_embed_media_html(&s) {
-                    in_img_to_raw_html.push(html.clone());
-
-                    vec![
-                        Event::Start(
-                            Container::RawInline { format: "html" },
-                            Attributes::try_from("{ loading=lazy }").expect("Can't fail"),
-                        ),
-                        Event::Str(html.into()),
-                    ]
-                } else {
-                    vec![Event::Start(
-                        Container::Image(s, link_type),
-                        Attributes::try_from("{ loading=lazy }").expect("Can't fail"),
-                    )]
-                };
-                [
-                    vec![
-                        Event::Start(
-                            Container::Div {
-                                class: "lazyload-wrapper",
-                            },
-                            jotdown::Attributes::try_from(
-                                "{ onclick=\"this.classList.toggle('-expanded')\" }",
-                            )
-                            .expect("Can't fail"),
-                        ),
-                        Event::Start(Container::Paragraph, Attributes::new()),
-                        Event::Str(load_msg),
-                        Event::End(Container::Paragraph),
-                    ],
-                    img_events,
-                ]
-                .concat()
-            }
-            Event::End(Container::Image(s, link_type)) => {
-                vec![
-                    if let Some(_html) = in_img_to_raw_html.pop() {
-                        Event::End(Container::RawInline { format: "html" })
+                        ))]
                     } else {
-                        Event::End(Container::Image(s, link_type))
+                        vec![Event::End(Container::Link(s, jotdown::LinkType::AutoLink))]
+                    }
+                }
+                Event::Start(Container::Image(s, _link_type), _attr) => {
+                    if let Some(html) = maybe_embed_media_html(&s) {
+                        in_img_to_raw_html.push((html, String::new()));
+                    } else {
+                        in_img_to_img.push(String::new());
+                    };
+                    vec![Event::Start(
+                        Container::Div {
+                            class: "lazyload-wrapper",
+                        },
+                        jotdown::Attributes::try_from(
+                            "{ onclick=\"this.classList.add('-expanded')\" }",
+                        )
+                        .expect("Can't fail"),
+                    )]
+                }
+                Event::End(Container::Image(s, link_type)) => [
+                    if let Some((html, alt)) = in_img_to_raw_html.pop() {
+                        let alt = alt.trim();
+                        let load_msg = if alt.is_empty() {
+                            format!("Load: {}", s).into()
+                        } else {
+                            format!("Load “{}”: {}", alt, s).into()
+                        };
+                        vec![
+                            Event::Start(Container::Paragraph, Attributes::new()),
+                            Event::Str(load_msg),
+                            Event::End(Container::Paragraph),
+                            Event::Start(
+                                Container::RawInline { format: "html" },
+                                Attributes::try_from("{ loading=lazy }").expect("Can't fail"),
+                            ),
+                            Event::Str(html.into()),
+                            Event::End(Container::RawInline { format: "html" }),
+                        ]
+                    } else if let Some(alt) = in_img_to_img.pop() {
+                        let alt = alt.trim();
+                        let load_msg = if alt.is_empty() {
+                            format!("Load: {}", s).into()
+                        } else {
+                            format!("Load “{}”: {}", alt, s).into()
+                        };
+                        vec![
+                            Event::Start(Container::Paragraph, Attributes::new()),
+                            Event::Str(load_msg),
+                            Event::End(Container::Paragraph),
+                            Event::Start(
+                                Container::Image(s.clone(), link_type),
+                                Attributes::try_from("{ loading=lazy }").expect("Can't fail"),
+                            ),
+                            Event::Str(alt.to_string().into()),
+                            Event::End(Container::Image(s, link_type)),
+                        ]
+                    } else {
+                        panic!("Can't be here")
                     },
-                    Event::End(Container::Div {
+                    vec![Event::End(Container::Div {
                         class: "img-wrapper",
-                    }),
+                    })],
                 ]
+                .concat(),
+                Event::Str(s) => {
+                    if !in_profile_link.is_empty() {
+                        let profile = in_profile_link.last().expect("Not empty just checked");
+                        vec![Event::Str(format!("@{profile}").into())]
+                    } else if let Some(last) = in_img_to_raw_html.last_mut() {
+                        last.1 = s.to_string();
+                        // skip the img alt tag
+                        vec![]
+                    } else if let Some(last) = in_img_to_img.last_mut() {
+                        *last = s.to_string();
+                        // skip the img alt tag
+                        vec![]
+                    } else {
+                        vec![Event::Str(s)]
+                    }
+                }
+                event => vec![event],
             }
-            Event::Str(_s) if !in_profile_link.is_empty() => {
-                let profile = in_profile_link.last().expect("Not empty just checked");
-                vec![Event::Str(format!("@{profile}").into())]
-            }
-            event => vec![event],
         }));
 
         PreEscaped(out)
